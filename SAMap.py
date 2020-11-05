b784b0b71614856fca7dbe05ce706b732cd91b91
atarashansky <tarashan@stanford.edu>
2020-11-04 16:09:27 -0800

    fix

--- SAMap.py
+++ SAMap.py
@@ -1027,7 +1027,7 @@ class Samap(object):
 
         gnnm2 = get_pairs(sam1,sam2,gnnm,gn1,gn2,NOPs1=NOPs1,NOPs2=NOPs2)
         sam_def = samap([sam1,sam2],gnnm2,gn, umap=False, NH1=NH1, NH2=NH2,
-                                    coarsen=coarsen,K=K)
+                                    coarsen=True,K=K)
         self.sam_def = sam_def
         sam4=sam_def
 
@@ -1041,7 +1041,7 @@ class Samap(object):
         self.GNNMS_pruned=[gnnm2]
         i=0
         self.GNNMS_nnm=[sam_def.adata.obsp['connectivities']]
-        while i < NUMITERS:
+        while i < NUMITERS-1:
             print('ITERATION: ' + str(i),
                   '\nAverage alignment score (A.S.): ',avg_as(sam4).mean(),
                   '\nMax A.S. improvement:',np.max(new-old),
