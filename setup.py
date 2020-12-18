f3ca5bc27fefa185a992ddc16e0f31f88fab63cd
atarashansky <tarashan@stanford.edu>
2020-12-18 14:31:40 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author="Alexander J. Tarashansky",
     author_email="tarashan@stanford.edu",
     keywords="scrnaseq analysis manifold reconstruction cross-species mapping",
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     py_modules=["SAMap"],    
     install_requires=[
         "sam-algorithm==0.7.7",
