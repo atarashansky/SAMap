a4ca20cd0af56e476109b463c0f5b767c21e30d4
atarashansky <tarashan@stanford.edu>
2020-12-26 12:14:17 -0800

    updated map_genes

--- map_genes.sh
+++ map_genes.sh
@@ -110,24 +110,28 @@ fi
 
 if [[ "$t1" == "nucl" && "$t2" == "nucl" ]]
 then
-tblastx -query $tr1 -db $tr2 -outfmt 6 -out "maps/${n1}${n2}/${n1}_to_${n2}.txt" -num_threads 4 -max_hsps 1 -evalue 1e-6 &
-tblastx -query $tr2 -db $tr1 -outfmt 6 -out "maps/${n1}${n2}/${n2}_to_${n1}.txt" -num_threads 4 -max_hsps 1 -evalue 1e-6 &
+echo "Running tblastx in both directions"
+tblastx -query $tr1 -db $tr2 -outfmt 6 -out "maps/${n1}${n2}/${n1}_to_${n2}.txt" -num_threads 8 -max_hsps 1 -evalue 1e-6
+tblastx -query $tr2 -db $tr1 -outfmt 6 -out "maps/${n1}${n2}/${n2}_to_${n1}.txt" -num_threads 8 -max_hsps 1 -evalue 1e-6
 fi
 
 if [[ "$t1" == "nucl" && "$t2" == "prot" ]]
 then
-blastx -query $tr1 -db $tr2 -outfmt 6 -out "maps/${n1}${n2}/${n1}_to_${n2}.txt" -num_threads 4 -max_hsps 1 -evalue 1e-6 &
-tblastn -query $tr2 -db $tr1 -outfmt 6 -out "maps/${n1}${n2}/${n2}_to_${n1}.txt" -num_threads 4 -max_hsps 1 -evalue 1e-6 &
+echo "Running blastx from 1 to 2 and tblastn from 2 to 1"
+blastx -query $tr1 -db $tr2 -outfmt 6 -out "maps/${n1}${n2}/${n1}_to_${n2}.txt" -num_threads 8 -max_hsps 1 -evalue 1e-6
+tblastn -query $tr2 -db $tr1 -outfmt 6 -out "maps/${n1}${n2}/${n2}_to_${n1}.txt" -num_threads 8 -max_hsps 1 -evalue 1e-6
 fi
 
 if [[ "$t1" == "prot" && "$t2" == "nucl" ]]
 then
-tblastn -query $tr1 -db $tr2 -outfmt 6 -out "maps/${n1}${n2}/${n1}_to_${n2}.txt" -num_threads 4 -max_hsps 1 -evalue 1e-6 &
-blastx -query $tr2 -db $tr1 -outfmt 6 -out "maps/${n1}${n2}/${n2}_to_${n1}.txt" -num_threads 4 -max_hsps 1 -evalue 1e-6 &
+echo "Running tblastn from 1 to 2 and blastx from 2 to 1"
+tblastn -query $tr1 -db $tr2 -outfmt 6 -out "maps/${n1}${n2}/${n1}_to_${n2}.txt" -num_threads 8 -max_hsps 1 -evalue 1e-6
+blastx -query $tr2 -db $tr1 -outfmt 6 -out "maps/${n1}${n2}/${n2}_to_${n1}.txt" -num_threads 8 -max_hsps 1 -evalue 1e-6
 fi
 
 if [[ "$t1" == "prot" && "$t2" == "prot" ]]
 then
-blastp -query $tr1 -db $tr2 -outfmt 6 -out "maps/${n1}${n2}/${n1}_to_${n2}.txt" -num_threads 4 -max_hsps 1 -evalue 1e-6 &
-blastp -query $tr2 -db $tr1 -outfmt 6 -out "maps/${n1}${n2}/${n2}_to_${n1}.txt" -num_threads 4 -max_hsps 1 -evalue 1e-6 &
+echo "Running blastp in both directions"
+blastp -query $tr1 -db $tr2 -outfmt 6 -out "maps/${n1}${n2}/${n1}_to_${n2}.txt" -num_threads 8 -max_hsps 1 -evalue 1e-6
+blastp -query $tr2 -db $tr1 -outfmt 6 -out "maps/${n1}${n2}/${n2}_to_${n1}.txt" -num_threads 8 -max_hsps 1 -evalue 1e-6
 fi
