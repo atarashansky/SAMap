82a98afd9c2f399a9799ce2ad1d9f10fa48fac88
atarashansky <atarashansky@chanzuckerberg.com>
2022-08-01 13:43:21 -0400

    test

--- samap/analysis.py
+++ samap/analysis.py
@@ -129,7 +129,7 @@ def GOEA(target_genes,GENE_SETS,df_key='GO',goterms=None,fdr_thresh=0.25,p_thres
     print("DEBUGGING_LINE")
     print(enriched_goterms)
     print(fdr_q_probs)
-    enriched_goterms = pd.DataFrame(data=fdr_q_probs,index=enriched_goterms,columns=['fdr_q_value'])
+    enriched_goterms = pd.DataFra(data=fdr_q_probs,index=enriched_goterms,columns=['fdr_q_value'])
     enriched_goterms['p_value'] = p_values
     enriched_goterms['genes'] = gns
     
@@ -235,7 +235,7 @@ class FunctionalEnrichment(object):
             index.extend([RES.index[i]]*len(l))
         
         RES = pd.DataFrame(index = index,data = data,columns = ['GO'])
-        
+        print(RES)
         genes = np.array(list(RES.index))
         agt = np.array(list(RES['GO'].values))
         idx = np.argsort(agt)
